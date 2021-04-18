c70a09fe38a708a87afe056aa486639d9a9674f4
Brandon Liu <bdon@bdon.org>
2021-04-18 21:49:28 +0800

    add potpack, change file ext to plain html

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
+// https://github.com/mapbox/potpack
+// ISC License
+//
+// Copyright (c) 2018, Mapbox
+//
+// Permission to use, copy, modify, and/or distribute this software for any purpose
+// with or without fee is hereby granted, provided that the above copyright notice
+// and this permission notice appear in all copies.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+// OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+// TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+// THIS SOFTWARE.
+let potpack = boxes => {
+    let area = 0
+    let maxWidth = 0
+
+    for (const box of boxes) {
+        area += box.w * box.h
+        maxWidth = Math.max(maxWidth, box.w)
+    }
+    boxes.sort((a, b) => b.h - a.h)
+    const startWidth = Math.max(Math.ceil(Math.sqrt(area / 0.95)), maxWidth)
+    const spaces = [{x: 0, y: 0, w: startWidth, h: Infinity}]
+
+    let width = 0
+    let height = 0
+
+    for (const box of boxes) {
+        for (let i = spaces.length - 1; i >= 0; i--) {
+            const space = spaces[i]
+            if (box.w > space.w || box.h > space.h) continue
+            box.x = space.x
+            box.y = space.y
+
+            height = Math.max(height, box.y + box.h)
+            width = Math.max(width, box.x + box.w)
+
+            if (box.w === space.w && box.h === space.h) {
+                const last = spaces.pop()
+                if (i < spaces.length) spaces[i] = last
+
+            } else if (box.h === space.h) {
+                space.x += box.w
+                space.w -= box.w
+
+            } else if (box.w === space.w) {
+                space.y += box.h
+                space.h -= box.h
+
+            } else {
+                spaces.push({
+                    x: space.x + box.w,
+                    y: space.y,
+                    w: space.w - box.w,
+                    h: box.h
+                })
+                space.y += box.h
+                space.h -= box.h
+            }
+            break
+        }
+    }
+
+    return {
+        w: width,
+        h: height,
+        fill: (area / (width * height)) || 0
+    }
+}
 
 let mkimg = src => {
   return new Promise((resolve, reject) => {
@@ -14,29 +87,31 @@ export default class Protosprites {
     }
 
     async createCanvas(src) {
-        let canvas = document.createElement('canvas')
-        canvas.width = 512
-        canvas.height = 512
-        let ctx = canvas.getContext('2d')
-
         let tree
-        if (src.endsWith('.psprites')) {
+        let scale = window.devicePixelRatio
+        if (src.endsWith('.html')) {
             let c = await fetch(src)
             let str = await c.text()
             tree = (new window.DOMParser()).parseFromString(str, "text/html")
         }
-
         this.mapping = {}
-
-        let x = 0
-        // this happens in series, probably nothing to be gained from Promise.all
+        let boxes = []
         for (let ps of tree.body.children) {
             var svg64 = btoa(new XMLSerializer().serializeToString(ps))
             var image64 = 'data:image/svg+xml;base64,' + svg64
             let img = await mkimg(image64)
-            ctx.drawImage(img,x,0)
-            this.mapping[ps.id] = {x:x,y:0,width:img.width,height:img.height}
-            x += img.width
+            boxes.push({w:img.width*scale,h:img.height*scale,img:img,id:ps.id})
+        }
+
+        let packresult = potpack(boxes)
+        let canvas = document.createElement('canvas')
+        canvas.width = packresult.w
+        canvas.height = packresult.h 
+        let ctx = canvas.getContext('2d')
+
+        for (let box of boxes) {
+            ctx.drawImage(box.img,box.x,box.y,box.w,box.h)
+            this.mapping[box.id] = {x:box.x,y:box.y,w:box.w,h:box.h}
         }
         return canvas
     }
@@ -44,9 +119,8 @@ export default class Protosprites {
     async get(name) {
         let canvas = await this.canvas
         let result = this.mapping[name]
+        if (!result) throw new Error(name + " not found")
         result.canvas = canvas
         return result
     }
 }
-
-
